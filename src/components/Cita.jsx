d222c71befff6f41d908245ce647adf84e3c1399
Uriel Engelberg & Lucas Tjor <uelt@localhost>
2025-04-21 14:30:34 -0300

    Comienzo fuerte

---
+++ src/components/Cita.jsx
@@ -0,0 +1,12 @@
+export default function Cita() {
+    return (
+        <div className="cita">
+            <p>Mascota: <span>Nina</span></p>
+            <p>Dueño: <span>Martin</span></p>
+            <p>Fecha: <span>2021-08-05</span></p>
+            <p>Hora: <span>08:20</span></p>
+            <p>Sintomas: <span>Le duele la pierna</span></p>
+            <button className="button elimnar u-full-width">Eliminar ×</button>
+        </div>
+    );
+};
---
+++ src/components/Listado.jsx
@@ -0,0 +1,13 @@
+import Cita from "./Cita";
+
+export default function Listado() {
+    return (
+        <div className="one-half column">
+            <h2>Administra tus citas</h2>
+            <Cita />
+            <Cita />
+            <Cita />
+            <Cita />
+        </div>
+    );
+}
