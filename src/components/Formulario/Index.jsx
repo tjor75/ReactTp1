915fef9ff8d402da3bb80e7b8296d1676dab40b5
Lucas Tjor <tjor75@gmail.com>
2025-05-18 16:50:27 -0300

    Cargar citas

--- src/components/Formulario/Index.jsx
+++ src/components/Formulario/Index.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Cita from '../../utils/Cita';
 import './style.css';
 
 export default function Formulario({ setCitas }) {
@@ -9,8 +10,9 @@ export default function Formulario({ setCitas }) {
     const [sintomas, setSintomas] = useState('');
     const [error, setError] = useState(false);
 
-    const agregarCita = () => {
-        console.log(hora, fecha);
+    const agregarCita = (e) => {
+        e.preventDefault();
+        setCitas(prev => [...prev, new Cita(mascota.value, propietario.value, fecha.target.value, hora.target.value, sintomas.value)]);
         //if (mascota !== '' || propietario !== '' || fecha !== '' || hora >)
         //setCitas([])
     };
@@ -18,18 +20,18 @@ export default function Formulario({ setCitas }) {
     return (
         <div className="one-half column">
             <h2>Crear mi Cita</h2>
-            <form>
+            <form onSubmit={agregarCita}>
                 <label>Nombre Mascota</label>
                 <input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" value={mascota.value} onChange={setMascota} />
                 <label>Nombre Dueño</label>
-                <input type="text" name="propietario" className="u-full-width" placeholder="Nombre dueño de la mascota" value={propietario} onChange={setPropietario} />
+                <input type="text" name="propietario" className="u-full-width" placeholder="Nombre dueño de la mascota" value={propietario.value} onChange={setPropietario} />
                 <label>Fecha</label>
-                <input type="date" name="fecha" className="u-full-width" value={fecha} onChange={setFecha} />
+                <input type="date" name="fecha" className="u-full-width" value={fecha.value} onChange={setFecha} />
                 <label>hora</label>
-                <input type="time" name="hora" className="u-full-width" value={hora} onChange={setHora} />
+                <input type="time" name="hora" className="u-full-width" value={hora.value} onChange={setHora} />
                 <label>Sintomas</label>
-                <textarea name="sintomas" className="u-full-width" value={sintomas} onChange={setSintomas} />
-                <button type="submit" className="u-full-width button-primary" onClick={agregarCita}>Agregar Cita</button>
+                <textarea name="sintomas" className="u-full-width" value={sintomas.value} onChange={setSintomas} />
+                <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
             </form>
         </div>
     );
---
+++ src/utils/Cita.js
@@ -0,0 +1,9 @@
+export default class Cita {
+    constructor(mascota, propietario, fecha, hora, sintomas) {
+        this.mascota = mascota;
+        this.propietario = propietario;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.sintomas = sintomas;
+    }
+}
